5dcb1a3cb09408f564980677722424b06693bc40
James Miller <jamesstewartmiller@gmail.com>
2022-05-13 11:32:29 +0100

    humble beginnings...

--- django_restic/admin.py
+++ django_restic/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.urls import path
 
-# Register your models here.
+from . import views
+
+class CustomAdminSite(admin.AdminSite):
+
+    def get_urls(self):
+        custom_urls = [
+            path('preferences/', self.admin_view(views.my_view)),
+        ]
+
+        return custom_urls + admin.site.urls[0] # custom urls must be at the beginning
+
+
+site = CustomAdminSite()
+
+
+
+# you can register your models on this site object as usual, if needed
